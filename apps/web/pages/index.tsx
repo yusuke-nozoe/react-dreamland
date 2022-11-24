29e6838bcd9197686fcb610b29ec32318a27659d
Yusuke Nozoe <yusuke.nozoe@gmail.com>
2022-11-24 17:03:16 +0900

    Do not disable hoist

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,14 @@
-import Button from "client/Button";
+import ClientButton from "client/Button";
+import { Button } from "ui";
+import { Pressable, Text } from 'react-native'
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
-      <Button />
+      <Pressable onPress={() => alert('hoge')}><Text>BUTTON</Text></Pressable>
+      <ClientButton />
+      <Text>hoge</Text>
     </div>
   );
 }
