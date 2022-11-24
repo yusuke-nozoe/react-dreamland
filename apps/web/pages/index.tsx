a2edd9e2d8cdd1d857c7dce3f50eb594137a789c
Yusuke Nozoe <yusuke.nozoe@gmail.com>
2022-11-24 18:02:24 +0900

    nicely display same screen

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,14 +1,8 @@
-import ClientButton from "client/Button";
-import { Button } from "ui";
+import HomeScreen from "client/screens/Home.screen";
 import { Pressable, Text } from 'react-native'
 
 export default function Web() {
   return (
-    <div>
-      <h1>Web</h1>
-      <Pressable onPress={() => alert('hoge')}><Text>BUTTON</Text></Pressable>
-      <ClientButton />
-      <Text>hoge</Text>
-    </div>
+    <HomeScreen />
   );
 }
