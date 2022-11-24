cef03396e0b0aedb0196ee79706b1314d1a73e74
Yusuke Nozoe <yusuke.nozoe@gmail.com>
2022-11-24 17:51:59 +0900

    nicely display HomeScreen

---
+++ packages/client/screens/Home.screen.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+
+import styles from './Home.styles'
+
+const HomeScreen = () => {
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textLabel}>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
---
+++ packages/client/screens/Home.styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textLabel: {
+    fontSize: 24,
+  }
+})
+
+export default styles
