a3f588cc53aa87a5e35c6d80eeb199ba2217e020
Yusuke Nozoe <yusuke.nozoe@gmail.com>
2022-11-25 15:45:41 +0900

    add renderer

---
+++ apps/desktop/preload.js
@@ -0,0 +1,8 @@
+const { contextBridge } = require('electron')
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  // 関数だけでなく、変数も公開できます
+})
